58d11b8135463825e2bf7c159e79bb5a874a6a1b
EgMeln <Egmell@yandex.ru>
2020-10-15 20:38:28 +0300

    If I understand correctly.

--- src/main/java/com/bsu/by/Company.java
+++ src/main/java/com/bsu/by/Company.java
@@ -1,6 +1,7 @@
 package com.bsu.by;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Company {
     private String name;
@@ -17,7 +18,26 @@ public class Company {
     private String internetAddress;
     private String[] str;
 
+    public Company(String name, String shortName, String actualData, String address, String foundDate, String amountEmployee,
+                   String auditor, String number, String email, String industry, String activity, String internetAddress){
+        this.name = name;
+        this.shortName = shortName;
+        this.actualData = actualData;
+        this.address = address;
+        this.foundDate = foundDate;
+        this.amountEmployee = amountEmployee;
+        this.auditor = auditor;
+        this.number= number;
+        this.email = email;
+        this.industry = industry;
+        this.activity = activity;
+        this.internetAddress = internetAddress;
+    }
+
     Company(String[] str) throws IOException {
+        if (str.length != 12) {
+            throw new IllegalArgumentException("Error: wrong number of fields");
+        }
         this.str = str;
         this.name = str[0];
         this.shortName = str[1];
@@ -33,7 +53,26 @@ public class Company {
         this.internetAddress = str[11];
     }
 
-    String[] getStr() {
+    public String[] getStr() {
         return str;
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, shortName, actualData, address, foundDate, amountEmployee, auditor, number, email, industry, activity, internetAddress);
+    }
+
+    public String toString() {
+        return name + ";" + shortName + ";" + actualData + ";" + address + ";" + foundDate + ";" + amountEmployee + ";"
+                + auditor + ";" + number + ";" + email + ";" + industry + ";" + activity + ";" + internetAddress + ";";
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return Objects.equals(name, company.name) && Objects.equals(shortName, company.shortName) && Objects.equals(actualData, company.actualData) &&
+                Objects.equals(address, company.address) && Objects.equals(foundDate, company.foundDate) && Objects.equals(amountEmployee, company.amountEmployee) &&
+                Objects.equals(auditor, company.auditor) && Objects.equals(number, company.number) && Objects.equals(email, company.email) && Objects.equals(industry, company.industry) &&
+                Objects.equals(activity, company.activity) && Objects.equals(internetAddress, company.internetAddress);
     }
 }
