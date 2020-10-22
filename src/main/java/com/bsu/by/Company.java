d01c24162a1003aedd2a308f45ee90163a7373fb
EgMeln <Egmell@yandex.ru>
2020-10-22 21:36:08 +0300

    If I understand correctly.

--- src/main/java/com/bsu/by/Company.java
+++ src/main/java/com/bsu/by/Company.java
@@ -1,15 +1,19 @@
 package com.bsu.by;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Objects;
+import java.util.Date;
 
 public class Company {
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
     private String name;
     private String shortName;
-    private String actualData;
+    private Date actualData;
     private String address;
-    private String foundDate;
-    private String amountEmployee;
+    private Date foundDate;
+    private int amountEmployee;
     private String auditor;
     private String number;
     private String email;
@@ -18,8 +22,8 @@ public class Company {
     private String internetAddress;
     private String[] str;
 
-    public Company(String name, String shortName, String actualData, String address, String foundDate, String amountEmployee,
-                   String auditor, String number, String email, String industry, String activity, String internetAddress){
+    public Company(String name, String shortName, Date actualData, String address, Date foundDate, int amountEmployee,
+                   String auditor, String number, String email, String industry, String activity, String internetAddress) {
         this.name = name;
         this.shortName = shortName;
         this.actualData = actualData;
@@ -27,30 +31,50 @@ public class Company {
         this.foundDate = foundDate;
         this.amountEmployee = amountEmployee;
         this.auditor = auditor;
-        this.number= number;
+        this.number = number;
         this.email = email;
         this.industry = industry;
         this.activity = activity;
         this.internetAddress = internetAddress;
     }
 
-    Company(String[] str) throws IOException {
+    Company(String[] str) throws IOException, ParseException {
         if (str.length != 12) {
             throw new IllegalArgumentException("Error: wrong number of fields");
         }
         this.str = str;
         this.name = str[0];
         this.shortName = str[1];
-        this.actualData = str[2];
+        this.actualData = dateFormat.parse(str[2]);
         this.address = str[3];
-        this.foundDate = str[4];
-        this.amountEmployee = str[5];
+        this.foundDate = dateFormat.parse(str[4]);
+        this.amountEmployee = Integer.parseInt(str[5]);
         this.auditor = str[6];
         this.number = str[7];
         this.email = str[8];
         this.industry = str[9];
         this.activity = str[10];
         this.internetAddress = str[11];
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public Date getFoundDate() {
+        return foundDate;
+    }
+
+    public int getAmountEmployee() {
+        return amountEmployee;
     }
 
     public String[] getStr() {
--- src/main/java/com/bsu/by/Main.java
+++ src/main/java/com/bsu/by/Main.java
@@ -1,32 +1,112 @@
 package com.bsu.by;
 
+import javax.annotation.processing.Processor;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
+    public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static final String LOG_FILE_NAME = "logfile.txt";
+
     public static void main(String[] args) throws IOException {
         ArrayList<Company> companyList = new ArrayList<>();
         Path path = Paths.get("InputText.txt");
         FileWriter fw = new FileWriter("OutputText.txt", false);
+        LOGGER.setLevel(Level.FINE);
+        FileHandler fh = new FileHandler(LOG_FILE_NAME, true);
+        fh.setLevel(Level.FINE);
+        fh.setFormatter(new SimpleFormatter());
+        LOGGER.addHandler(fh);
+        LOGGER.fine("Program started...\n");
         try (Scanner scanner = new Scanner(path)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] company = line.split(";");
                 companyList.add(new Company(company));
             }
-            for (Company i : companyList) {
+            List<Company> foundCompanies = getByRequest(scanner, companyList);
+            for (Company i : foundCompanies) {
                 for (String j : i.getStr()) {
                     fw.write(j + ";");
                 }
                 fw.write("\n");
             }
-        } catch (IOException a) {
+        } catch (IOException | ParseException a) {
             System.out.println(a.getMessage());
         }
         fw.close();
     }
+
+    public static void showRequest() {
+        System.out.println("1.by short name");
+        System.out.println("2.by department of work");
+        System.out.println("3.by activity kind");
+        System.out.println("4.by foundation date (between DATE1 && DATE2) [dd.MM.yyyy]");
+        System.out.println("5.by employees number (between NUM1 && NUM2)");
+        System.out.println();
+    }
+
+    static List<Company> getByRequest(Scanner scanner, List<Company> companyList) throws IllegalArgumentException, ParseException {
+        List<Company> result = new ArrayList<>();
+        String requestString;
+        try (Scanner sc = new Scanner(System.in)) {
+            while (true) {
+                showRequest();
+                System.out.println("Enter request: ");
+                switch (sc.nextInt()) {
+                    case 1:
+                        System.out.println("Enter shortname: ");
+                        String shortName = sc.next();
+                        requestString = shortName;
+                        result = Search.findByShortName(companyList, shortName);
+                        break;
+                    case 2:
+                        System.out.println("Enter industry");
+                        String industry = sc.next();
+                        requestString = industry;
+                        result = Search.findByIndustry(companyList, industry);
+                        break;
+                    case 3:
+                        System.out.println("Enter kind of activity: ");
+                        String activity = sc.next();
+                        requestString = activity;
+                        result = Search.findByActivity(companyList, activity);
+                        break;
+                    case 4:
+                        System.out.println("Enter first dates: ");
+                        requestString = sc.next();
+                        Date firstDate = Company.dateFormat.parse(requestString);
+                        System.out.println("Enter second dates: ");
+                        requestString = sc.next();
+                        Date secondDate = Company.dateFormat.parse(requestString);
+                        result = Search.findByFoundationDate(companyList, firstDate, secondDate);
+                        break;
+                    case 5:
+                        System.out.println("Enter first numbers: ");
+                        requestString = sc.next();
+                        int firstNumber = Integer.parseInt(requestString);
+                        System.out.println("Enter second dates: ");
+                        requestString = sc.next();
+                        int secondNumber = Integer.parseInt(requestString);
+                        result = Search.findByAmountEmployee(companyList, firstNumber, secondNumber);
+                        break;
+                    default:
+                        return result;
+                }
+                Main.LOGGER.fine("Find company " + " :" + requestString + ", Companies found: " + result.size() + "\n");
+            }
+        }
+    }
 }
+
